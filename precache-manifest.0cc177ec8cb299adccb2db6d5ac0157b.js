64d0db797fa87ed0befe92489606d7928871cf8e
tanyjnaaman <67091469+tanyjnaaman@users.noreply.github.com>
2021-08-14 16:00:35 +0800

    Updates

---
+++ precache-manifest.0cc177ec8cb299adccb2db6d5ac0157b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6747a62965934b2d932155ac586f105",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ecfc6957a9d66a06b3c",
+    "url": "/static/css/main.d5e1e85d.chunk.css"
+  },
+  {
+    "revision": "b3188d4410310cae616b",
+    "url": "/static/js/2.c71e60cc.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.c71e60cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ecfc6957a9d66a06b3c",
+    "url": "/static/js/main.3a745946.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "fe6263ff86f41860bcf7cecc7ab7fb09",
+    "url": "/static/media/DSO.fe6263ff.png"
+  },
+  {
+    "revision": "09cba629fb0ccc1613fe26796de07d22",
+    "url": "/static/media/DarkNUS.09cba629.png"
+  },
+  {
+    "revision": "19e5d5932663f0ac52e477921d6acd8e",
+    "url": "/static/media/LumiNUS.19e5d593.png"
+  },
+  {
+    "revision": "060e05f339e6155be03b6f643b9bc496",
+    "url": "/static/media/MTS.060e05f3.jpg"
+  },
+  {
+    "revision": "dc4d529b8481f1abe630bf6752170e18",
+    "url": "/static/media/NYX.dc4d529b.png"
+  },
+  {
+    "revision": "dfcd3d45f3c94d75fe82803d01e74777",
+    "url": "/static/media/Rio.dfcd3d45.png"
+  },
+  {
+    "revision": "911f1b52ef63a143727e205aeeefe2e8",
+    "url": "/static/media/iti.911f1b52.png"
+  },
+  {
+    "revision": "b729af804301b1b73080e7726f1f12b0",
+    "url": "/static/media/me.b729af80.jpg"
+  },
+  {
+    "revision": "d02425bdf4dff54e202fa2b56625a773",
+    "url": "/static/media/myResume.d02425bd.pdf"
+  },
+  {
+    "revision": "239b1cfb38d768a69832a32b624a3414",
+    "url": "/static/media/rover.239b1cfb.png"
+  },
+  {
+    "revision": "9031d525e2925139f411ae92cdeb9d98",
+    "url": "/static/media/shell.9031d525.jpg"
+  }
+]);
